16009b9fe80fcf51307fd4477aced8cc500fd2ab
sermarcel <marcin.brzescianski@gmail.com>
2017-03-19 20:52:04 +0100

    add to admin

--- film_test/film_app/admin.py
+++ film_test/film_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from film_app.models import Movie
 
 # Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title',)
