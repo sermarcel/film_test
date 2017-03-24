fb9b6f5c148051d939d9704e2eb57cdf0d3f398e
sermarcel <marcin.brzescianski@gmail.com>
2017-03-24 22:52:42 +0100

    add static and public

--- film_test/film_app/views.py
+++ film_test/film_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, DeleteView, UpdateView, FormView
+from film_app.models import Movie
 
 # Create your views here.
+class AddMovie (CreateView):
+    model = Movie
+    fields = '__all__'
+    template_name = '/add_movie.html'
