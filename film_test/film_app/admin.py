8dd52a5033fe4c08542717968919d2af4e88c961
sermarcel <marcin.brzescianski@gmail.com>
2017-04-06 23:13:53 +0200

    admin

--- film_test/film_app/admin.py
+++ film_test/film_app/admin.py
@@ -5,3 +5,7 @@ from film_app.models import Movie
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('title',)
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name',)
