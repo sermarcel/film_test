691c71bae83d77b983dcf48ebc93e19bb210d8b9
sermarcel <marcin.brzescianski@gmail.com>
2017-04-11 23:52:29 +0200

    api from moviedb

--- film_test/film_app/views.py
+++ film_test/film_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, DeleteView, UpdateView, FormView
 from film_app.models import Movie
+import requests
 
 # Create your views here.
 class AddMovie (CreateView):
@@ -8,4 +9,17 @@ class AddMovie (CreateView):
     fields = '__all__'
     template_name = '/add_movie.html'
 
+class SearchMovie(models.Model):
 
+    parameters ={'api_key':'}
+    # open the url and the screen name 
+    # (The screen name is the screen name of the user for whom to return results for)#
+    #response = requests.get( "https://api.themoviedb.org/3/movie", params=parameters)
+    id=5
+    response = requests.get( 'https://api.themoviedb.org/3/movie/{}'.format(id), params=parameters)
+
+    data = response.json()
+
+
+    # print the result
+    print (data)
